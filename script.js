285f6c9c4f49b7019143eb31fd692f884680475f
Timothyl3aker <37913608+Timothyl3aker@users.noreply.github.com>
2018-04-03 23:02:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
- 'use strict';
+'use strict';
  
--const _ = require('lodash');
- const Script = require('smooch-bot').Script;
- 
--const scriptRules = require('./script.json');
--
- module.exports = new Script({
+const _ = require('lodash');
+const Script = require('smooch-bot').Script;
+const scriptRules = require('./script.json');
+module.exports = new Script({
      processing: {
--        //prompt: (bot) => bot.say('Beep boop...'),
-+        prompt: (bot) => bot.say('Beep boop...'),
+
+        prompt: (bot) => bot.say('Beep boop...'),
          receive: () => 'processing'
     },
     start: {
@@ -18,36 +16,36 @@
         }
     },
 askName: {
--        prompt: (bot) => bot.say('I can fill you in. What\'s your name?'),
-+      speak: {
+       prompt: (bot) => bot.say('I can fill you in. What\'s your name?'),
+         speak: {
          receive: (bot, message) => {
--            const name = message.text;
--            return bot.setProp('name', name)
--                .then(() => bot.say(`Great! Thanks ${name}
--                .then(() => 'giveExamples');
--        }
--    },
--        giveExamples: {
--        receive: (bot) => {
--        return bot.say('What would you like to know?'),
--                .then(() => 'reachOut');
--        }
--    },
--        reachOut: {
--        receive: (bot, message) => {
--        return bot.say('I vaca on most major messaging platforms, mail servers, sms and voice. Tell me which works for you.'),
--            const name = message.text;
--            return bot.setProp('name', name)
--                .then(() => bot.say('Would you like to reach out to Tim'}
--%[Skill Sets](postback:Skill Sets) %[Work History](postback:work history)`))
--                .then(() => 'finish');
--        }
--    },
+           const name = message.text;
+          return bot.setProp('name', name)
+               .then(() => bot.say(`Great! Thanks ${name}
+                .then(() => 'giveExamples');
+        }
+    },
+        giveExamples: {
+        receive: (bot) => {
+        return bot.say('What would you like to know?'),
+                .then(() => 'reachOut');
+        }
+    },
+        reachOut: {
+        receive: (bot, message) => {
+        return bot.say('I vaca on most major messaging platforms, mail servers, sms and voice. Tell me which works for you.'),
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say('Would you like to reach out to Tim'}
+%[Skill Sets](postback:Skill Sets) %[Work History](postback:work history)`))
+                .then(() => 'finish');
+        }
+    },
  
--    finish: {
--        receive: (bot, message) => {
--            return bot.getProp('name')
-+                .then((name) => bot.say(`Sorry ${name}, Tim didn't ` +
+    finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, Tim didn't ` +
                          'teach me how to do anything else!'))
                  .then(() => 'finish');
      } 
