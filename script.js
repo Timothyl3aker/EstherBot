e29726478d142e256343ff01f9f7692b1b091dd4
Timothyl3aker <37913608+Timothyl3aker@users.noreply.github.com>
2018-04-03 17:50:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,18 +10,31 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Tim\'s Career Bot!')
+            return bot.say('So you want to learn more about Tim...')
                 .then(() => 'askName');
         }
     },
-
     askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
+        prompt: (bot) => bot.say('I can fill you in. What\'s your name?'),
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
+                .then(() => bot.say(`Great! Thanks ${name}
+                .then(() => 'giveExamples');
+        }
+    },
+        giveExamples: {
+        prompt: (bot) => bot.say('It would be sweet to be human but I am just a bot. These are the career specific keywords Tim has taught me to repoond to.'),
+                .then(() => 'reachOut');
+        }
+    },
+        reachOut: {
+        prompt: (bot) => bot.say('I vaca on most major meesaging platforms, mail servers, sms and voice. Tell me which works for you.'),
+        receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say('Would you like to reach out to Tim'}
+%[Skill Sets](postback:Skill Sets) %[Work History](postback:work history)`))
                 .then(() => 'finish');
         }
     },
@@ -29,7 +42,7 @@ Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                .then((name) => bot.say(`Sorry ${name}, Tim didn't ` +
                         'teach me how to do anything else!'))
                 .then(() => 'finish');
         }
