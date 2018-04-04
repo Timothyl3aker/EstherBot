2ee70bf81763776bd32f0cf6570543589c40f26b
Timothyl3aker <37913608+Timothyl3aker@users.noreply.github.com>
2018-04-03 22:10:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,41 +11,56 @@ module.exports = new Script({
     start: {
         receive: (bot) => {
             return bot.say('So you want to learn more about Tim...')
-                .then(() => 'askName');
+                .then(() => '');
         }
     },
-    askName: {
-        prompt: (bot) => bot.say('I can fill you in. What\'s your name?'),
+      speak: {
         receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(`Great! Thanks ${name}
-                .then(() => 'giveExamples');
-        }
-    },
-        giveExamples: {
-        receive: (bot) => {
-        return bot.say('What would you like to know?'),
-                .then(() => 'reachOut');
-        }
-    },
-        reachOut: {
-        receive: (bot, message) => {
-        return bot.say('I vaca on most major messaging platforms, mail servers, sms and voice. Tell me which works for you.'),
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say('Would you like to reach out to Tim'}
-%[Skill Sets](postback:Skill Sets) %[Work History](postback:work history)`))
-                .then(() => 'finish');
-        }
-    },
 
-    finish: {
-        receive: (bot, message) => {
-            return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, Tim didn't ` +
-                        'teach me how to do anything else!'))
-                .then(() => 'finish');
+            let upperText = message.text.trim().toUpperCase();
+
+            function updateSilent() {
+                switch (upperText) {
+                    case "CONNECT ME":
+                        return bot.setProp("silent", true);
+                    case "DISCONNECT":
+                        return bot.setProp("silent", false);
+                    default:
+                        return Promise.resolve();
+                }
+            }
+
+            function getSilent() {
+                return bot.getProp("silent");
+            }
+
+            function processMessage(isSilent) {
+                if (isSilent) {
+                    return Promise.resolve("speak");
+                }
+
+                if (!_.has(scriptRules, upperText)) {
+                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                }
+
+                var response = scriptRules[upperText];
+                var lines = response.split('\n');
+
+                var p = Promise.resolve();
+                _.each(lines, function(line) {
+                    line = line.trim();
+                    p = p.then(function() {
+                        console.log(line);
+                        return bot.say(line);
+                    });
+                })
+
+                return p.then(() => 'speak');
+            }
+
+            return updateSilent()
+                .then(getSilent)
+                .then(processMessage);
         }
     }
 });
