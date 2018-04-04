f870bf3c7aeaf5434e4ac7c5016b39383ebce3e2
Timothyl3aker <37913608+Timothyl3aker@users.noreply.github.com>
2018-04-04 15:10:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ module.exports = new Script({
     
         reachOut: {
         receive: (bot) => {
-        return bot.say('Would you like me to reach out to Tim? Click then leave me your contact info. Only Tim will use it %[Yes](postback:yes) %[No](postback:no)`)
-                .then(() => bot.say(`Roger that ${name} Tim will contact you shortly.`) 
+        return bot.say(`Would you like me to reach out to Tim? Click then leave me your contact info. Only Tim will use it %[Yes](postback:yes) %[No](postback:no)`)
+                .then(() => bot.say(`Roger that ${name} Tim will contact you shortly.`)) 
                 .then(() => 'finish');
         }
     },
